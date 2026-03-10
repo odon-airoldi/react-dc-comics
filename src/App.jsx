8dc4bfe5d39c055ef79a6c9121d3989cfecfca33
odon-airoldi <odon.airoldi@gmail.com>
2026-03-10 16:27:28 +0100

    pass data to the child component via prop

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,81 @@ import AppMain from './components/AppMain'
 import AppFooter from './components/AppFooter'
 import AppSectionFeatured from './components/AppSectionFeatured'
 
+
+const primary_nav = [
+  {
+    id: '1',
+    text: 'Character',
+    url: '#',
+    active: false
+  },
+  {
+    id: '2',
+    text: 'Comics',
+    url: '#',
+    active: true
+  },
+  {
+    id: '3',
+    text: 'Movie',
+    url: '#',
+    active: false
+  },
+  {
+    id: '4',
+    text: 'TV',
+    url: '#',
+    active: false
+  },
+  {
+    id: '5',
+    text: 'Games',
+    url: '#',
+    active: false
+  },
+  {
+    id: '6',
+    text: 'Collectibles',
+    url: '#',
+    active: false
+  },
+  {
+    id: '7',
+    text: 'Videos',
+    url: '#',
+    active: false
+  },
+  {
+    id: '8',
+    text: 'Fans',
+    url: '#',
+    active: false
+  },
+  {
+    id: '9',
+    text: 'News',
+    url: '#',
+    active: false
+  },
+  {
+    id: '10',
+    text: 'Shop',
+    url: '#',
+    active: false
+  }
+]
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <AppHeader />
+      <AppHeader header_nav={primary_nav} />
       <AppMain />
       <AppSectionFeatured />
       <AppFooter />
     </>
   )
+
 }
 
 export default App
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -2,70 +2,7 @@ import logo from '../img/dc-logo.png'
 
 
 
-const nav_ul = [
-    {
-        id: '1',
-        text: 'Character',
-        url: '#',
-        active: false
-    },
-    {
-        id: '2',
-        text: 'Comics',
-        url: '#',
-        active: true
-    },
-    {
-        id: '3',
-        text: 'Movie',
-        url: '#',
-        active: false
-    },
-    {
-        id: '4',
-        text: 'TV',
-        url: '#',
-        active: false
-    },
-    {
-        id: '5',
-        text: 'Games',
-        url: '#',
-        active: false
-    },
-    {
-        id: '6',
-        text: 'Collectibles',
-        url: '#',
-        active: false
-    },
-    {
-        id: '7',
-        text: 'Videos',
-        url: '#',
-        active: false
-    },
-    {
-        id: '8',
-        text: 'Fans',
-        url: '#',
-        active: false
-    },
-    {
-        id: '9',
-        text: 'News',
-        url: '#',
-        active: false
-    },
-    {
-        id: '10',
-        text: 'Shop',
-        url: '#',
-        active: false
-    }
-]
-
-export default function AppHeader() {
+export default function AppHeader({ header_nav }) {
 
     return (
         <header>
@@ -76,9 +13,9 @@ export default function AppHeader() {
                     </a>
                     <ul className="d-flex text-uppercase list-unstyled mb-0">
                         {
-                            nav_ul.map(nav_li => (
-                                <li className="col-auto px-3" key={nav_li.id}>
-                                    <a className={`text-decoration-none d-flex align-items-center h-100 ${nav_li.active ? 'active' : ''}`} href={nav_li.url}>{nav_li.text}</a>
+                            header_nav.map(nav_item => (
+                                <li className="col-auto px-3" key={nav_item.id}>
+                                    <a className={`text-decoration-none d-flex align-items-center h-100 ${nav_item.active ? 'active' : ''}`} href={nav_item.url}>{nav_item.text}</a>
                                 </li>
                             ))
                         }
